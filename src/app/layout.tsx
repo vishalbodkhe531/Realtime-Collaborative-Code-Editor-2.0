ab041c59f1eb3c02900da35469d94f41ad72b0a2
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2025-12-14 17:42:54 +0530

    improve metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Providers } from "@/utils/Providers";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/styles/globals.css"
-// import { Providers } from "../utils/Providers";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -17,9 +16,54 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Collaborative Code Editor",
-  description: "Live Multi-User Code Collaboration",
+  title: {
+    default: "Collaborative Code Editor",
+    template: "%s | Collaborative Code Editor",
+  },
+  description:
+    "A real-time collaborative code editor that allows multiple developers to write, edit, and review code together instantly.",
+  keywords: [
+    "collaborative code editor",
+    "real-time code editor",
+    "pair programming",
+    "live coding",
+    "online IDE",
+    "developer collaboration",
+    "code sharing",
+  ],
+  authors: [{ name: "Vishal Bodkhe" }],
+  creator: "Vishal Bodkhe",
+  applicationName: "Collaborative Code Editor",
+
+  icons: {
+    icon: "/tab-logo.jpg",
+    shortcut: "/tab-logo.jpg",
+    apple: "/tab-logo.jpg",
+  },
+
+  openGraph: {
+    title: "Collaborative Code Editor",
+    description:
+      "Write and collaborate on code in real time with your team. Fast, secure, and built for modern developers.",
+    type: "website",
+    url: "https://real-time-collaborative-code-editor-ten.vercel.app/",
+    siteName: "Collaborative Code Editor",
+    images: [
+      {
+        url: "/code-editor-cover.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Collaborative Code Editor Preview",
+      },
+    ],
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
+
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { redirect } from "next/navigation";
 
 const page = () => {
   return redirect("/join-form");
-
 };
 
 export default page;
